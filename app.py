6f8235eb2a16af6eeb5678979a2301f6fdf56cb3
Andrew Yatsura <andrewyazura203@gmail.com>
2021-12-14 00:04:12 +0200

    Import Updater and os.path only if required in app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-import os.path
-
-from telegram.ext import Updater
-
 from brobank_bot import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
+    import os.path
+    from telegram.ext import Updater
+
     updater = Updater(dispatcher=app.dispatcher, workers=None)
 
     if not os.path.isfile(".bot-started"):
